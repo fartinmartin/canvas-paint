6587b63856329e7608edd4012819a6860b7623d4
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2022-10-09 14:23:41 -0700

    test gh-pages `/docs`

--- vite.demo.js
+++ vite.demo.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	base: "./",
 	build: {
-		outDir: "demo",
+		outDir: "docs",
 	},
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
